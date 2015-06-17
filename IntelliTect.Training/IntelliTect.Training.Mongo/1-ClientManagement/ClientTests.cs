17b07d552ecc38001bdd996f4a88e350faef64b7
Kelly Adams <kelly@intellitect.com>
2015-06-16 18:02:47 -0700

    First client test

---
+++ IntelliTect.Training/IntelliTect.Training.Mongo/1-ClientManagement/ClientTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntelliTect.Training.Mongo._1_ClientManagement
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void WhenClientIsRequestedTwiceItGivesTheSameClient()
+        {
+            var result = Mongo.Client;
+            var result2 = Mongo.Client;
+
+            Assert.AreSame( result, result2 );
+
+        }
+    }
+}
---
+++ IntelliTect.Training/IntelliTect.Training.Mongo/Mongo.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace IntelliTect.Training.Mongo
+{
+    public static class Mongo
+    {
+        static Mongo()
+        {
+            Client = new MongoClient(ConfigurationManager.ConnectionStrings["Mongo"].ConnectionString);
+            TrainingDatabase = Client.GetDatabase( "mongo-training" );
+            RawCollection = TrainingDatabase.GetCollection<BsonDocument>( "example" );
+        }
+
+        public static IMongoClient Client { get; private set; }
+
+        public static IMongoDatabase TrainingDatabase { get; private set; }
+
+        public static IMongoCollection<BsonDocument> RawCollection { get; private set; }
+
+    }
+}
